18c0733f2b5aea63ba15cb4483e0a27c8ae73dd9
Tedla610 <56671065+Tedla610@users.noreply.github.com>
2024-02-20 11:30:05 -0800

    Addition

---
+++ src/address/MenuTest.java
@@ -0,0 +1,50 @@
+package address;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void prompt_FirstName() {
+    }
+
+    @Test
+    void prompt_LastName() {
+    }
+
+    @Test
+    void prompt_Street() {
+    }
+
+    @Test
+    void prompt_City() {
+    }
+
+    @Test
+    void prompt_State() {
+    }
+
+    @Test
+    void prompt_Zip() {
+    }
+
+    @Test
+    void prompt_Telephone() {
+    }
+
+    @Test
+    void prompt_Email() {
+    }
+}
